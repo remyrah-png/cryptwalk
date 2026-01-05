007095ca048d747e5376e07241394211e84ea5ae
remyrah-png <remyrah-png@users.noreply.github.com>
2026-01-05 14:40:14 -0500

    Remove combat system, player, item, and test classes; refactor project structure

--- player.py
+++
@@ -1,9 +0,0 @@
-# player.py
-
-from entity import CombatEntity
-
-class Player(CombatEntity):
-    def __init__(self):
-        super().__init__("Hero", 35, 35, 10, 5)
-        self.inventory = []
-        self.gold = 0
